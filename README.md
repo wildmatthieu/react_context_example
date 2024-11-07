e323f6eaa7b52a13214b048f91575b6ab0a7a6e7
spacecodeur <spacecodeur@gmail.com>
2024-11-07 19:19:38 +0100

    commit initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Utilisation de contexte dans react
+
+Hello ! Ce repo contient un exemple d'utilisation du contexte dans React. 
+Le but du jeu est de créer un début de tamagotchi contenant un écran principal et un écran "actions" permettant de nourrir le tamagotchi
---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import App from "./App.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/", // The root path
+    element: <App />, // Renders the App component for the home page
+  },
+]);
+
+const rootElement = document.getElementById("root");
+if (rootElement == null) {
+  throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
+}
+
+// Render the app inside the root element
+createRoot(rootElement).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
